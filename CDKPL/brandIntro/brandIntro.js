90a3aa857158d2741699e4a6442b9275b568e745
kpg1997 <kpg19979@gmail.com>
2021-01-29 21:55:05 +0900

    brandIntro(js, css) login(css,html,js) -->수정 forgothtml -->추가

--- CDKPL/brandIntro/brandIntro.js
+++ CDKPL/brandIntro/brandIntro.js
@@ -8,45 +8,6 @@ const brand = [
     { logo: 'GIORGIO ARMANI_LOGO', img: 'GIORGI_' }
 ]
 
-// $('#brand-panel').on( function () { //실행될 코드 })
-//     console.log('start');
-//     for (let k = 0; k < brand.length; k++) {
-//         var data = brand[k];
-//         document.write('<div class="brand-title-panel"><img class="brand-title-logo"></div>')
-//         for (let p = 0; p < 5; p++) {
-//         }
-//     }
-// })
-
-// $('.body').ready(function () {
-//     var $list = $('.list');
-//     for (let k = 0; k < brand.length; k++) {
-//         var data = brand[k];
-//         var item = createBrandTitle(data);
-//         $list.append(item); 
-//         for (let p = 0; p < 4; p++) {
-//             var img = createBrandItemsImg(data, p);
-//             $list.append(img);
-//         }
-//     }
-// })
-//  //
-
-// var $data = $('#brand-panel').clone().removeAttr('id');
-// function createBrandTitle(data) {
-
-//     console.log('./images/' + data.logo + '.jpg');
-//     $data.find('.brand-title-logo').attr('src', './images/' + data.logo + '.jpg');
-//     return $data;
-// }
-
-
-// function createBrandItemsImg(data, i) {
-//         var $item =  $('#brand-img-list').clone();
-//         console.log('/images/' + data.img + i + '.jpg');
-//         $item.find('#brand-img'+i+'').attr('src', './images/' + data.img + i + '.jpg');
-//         return $item;
-// }
 $('.body').ready(function () {
     var $list = $('.list');
 
--- CDKPL/login/login.js
+++ CDKPL/login/login.js
@@ -1,3 +1,4 @@
+const { emit } = require("process");
 
 function login() {
     var id = document.getElementById('input-id').value;
@@ -27,3 +28,40 @@ function find(num) {
 $(".menu-bar ul li").hover(function () {
     $(this).find("ul").stop().fadeToggle(300);
 });
+function find() {
+    // console.log(text);
+    window.open('./forgot.html', '_blank', "width=800,height=400,scrollbars=yes");
+}
+// forgot.html
+
+function findUserID() {
+    let name = document.getElementById('find-id-name').value;
+    let email = document.getElementById('find-id-email').value;
+    console.log(name,',',email)
+    if (name == '' || email == '') {
+        alert('다시 입력바랍니다.')
+    } else if (!validateEmail(email)) {
+        alert('이메일 기입을 정확히 해주시기 바랍니다.')
+    } else {
+        alert(name, ',', email)
+    }
+
+}
+
+function findUserPwd() {
+    let id = document.getElementById('find-pwd-id').value;
+    let name = document.getElementById('find-pwd-name').value;
+    let email = document.getElementById('find-pwd-email').value;
+    if (name == '' || email == '' || id == '') {
+        alert('다시 입력바랍니다.')
+    } else if (!validateEmail(email)) {
+        alert('이메일 기입을 정확히 해주시기 바랍니다.')
+    } else {
+        alert(id, name, email)
+    }
+
+}
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
