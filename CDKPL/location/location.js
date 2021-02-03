a88584ffda9052d8e7f0ba59f482e08da28bc54f
byeonggoon <qudrnsektzja@naver.com>
2021-02-03 11:20:29 +0900

    index.html 배경을 영상으로 바꾸고 / totalheader를 만들어 notice.html과 location.html 에 적용.

--- CDKPL/location/location.js
+++ CDKPL/location/location.js
@@ -1,3 +1,5 @@
-$(".menu-bar ul li").hover(function(){
-    $(this).find("ul").stop().fadeToggle(300);
+
+
+$(document).ready(function(){
+    $("#total-header").load("../total/totalheader.html")
 });
