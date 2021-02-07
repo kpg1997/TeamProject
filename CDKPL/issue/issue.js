375967673f23afe87304eb0d488715f617588324
byeonggoon <qudrnsektzja@naver.com>
2021-02-07 22:05:54 +0900

    issue페이지생성

---
+++ CDKPL/issue/issue.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#total-header").load("../total/totalheader.html")
+});
+
+$(document).ready(function(){
+    $("#total-footer").load("../total/totalfooter.html")
+});
